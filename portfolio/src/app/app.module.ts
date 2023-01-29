2d33422dc7ab92d4e8b4f96a15ed89ff2c828ce3
Ricardo Federico Barzola <fedebarzola7@gmail.com>
2023-01-29 18:10:19 -0300

    coloco otra vez el proyecto

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FondoComponent } from './components/fondo/fondo.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FondoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
